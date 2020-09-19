455ed30ecc8827790c546d715bb9e9618cfa0a62
Youjyun <Youjyun@Youjyun-PC>
2020-09-19 01:33:14 -0700

    commit

---
+++ 2020fall-info5100/src/Assignment2_2.java
@@ -0,0 +1,28 @@
+/**
+ * The deadline of this assignment is 09/25/2020 23:59 PST.
+ * Please feel free to contact Yafei and Yaqi for any questions.
+ */
+
+public class Assignment2_2 {
+    /**
+     * Given an integer array nums, find the contiguous subarray (containing at least one number)
+     * which has the largest sum and outprint its sum.
+     *
+     * Example 1:
+     * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+     * Output: 6
+     * Explanation: [4,-1,2,1] has the largest sum = 6.
+     *
+     * Example 2:
+     * Input: nums = [1]
+     * Output: 1
+     */
+    public int maxSubArray(int[] nums) {
+		return 0;
+        //write your code here
+    }
+
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
